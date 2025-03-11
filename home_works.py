3c845939fc1793f1c97fed8ba0e2595fc25eba74
Begimaijusupjan <begimaijusupjan@gmail.com>
2025-03-11 07:30:37 +0600

    Бул Бегимайдын уй тапшырмасы

---
+++ home_works.py
@@ -0,0 +1,29 @@
+n="Ali"
+m=25
+print(f"Hello {n},you are {m}")
+a=7
+b=4
+summ=a+b
+print(f"summa {a}and {b}={a+b}")
+m=[3,5,8,10,15]
+n=int(input("n: "))
+for i in m:
+    if i==n:
+        break
+
+    else:
+        print("dont founded")
+n=[7,9,13,18,21]
+for i in n:
+    if i%2==0:
+
+        print(i)
+n=input("n:")
+while True:
+    if n=="1234":
+        print("allowed")
+        break
+    else:
+        print("not allowed")
+
+
