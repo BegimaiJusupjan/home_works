1b0a65afc068eb9bb8d773d4d2a87b6e19392953
Begimaijusupjan <begimaijusupjan@gmail.com>
2025-03-13 21:00:11 +0600

    Function tema

--- home_works.py
+++ home_works.py
@@ -1,2 +1,33 @@
+def func():
+    print("python")
+    print("jawa")
+    print("git")
+    print("#"* 10)
+#f=func
+#f()
+# pr=print
+# pr("My names Arsen")
+# i=input
+def area_circle():
+    radius=float(input("Enter the radius"))
+    s=3,14*radius**2
+    print("Ayant",s)
 
+def area_rectangle(width,height):
+    if width<0 or height<0:
+        print("it is imposible")
+    else:
 
+        area=width*height
+        print("ayant",area)
+#area_rectangle(10,11)
+def info():
+    info="Arsen"
+    print("Your name is ",info)
+#info()
+def info(name):
+    print(f"your name is {name}")
+#info("Arsen")
+#info("Adil")
+name=input("Enter name:")
+info(name)
