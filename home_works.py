e7c8ba12748280e51d84c63dcc5a9f07a0a4fac2
Begimaijusupjan <begimaijusupjan@gmail.com>
2025-03-13 20:11:30 +0600

    ochty

--- home_works.py
+++ home_works.py
@@ -1,16 +1,2 @@
-n=[i for i in range(1,21) if i%2==0]
-print(*n)
-m=[i**2 for i in range(1,11)]
-print(*m)
-b=[5,12,7,18,3,10,8]
-res=(i for i in b if i>7)
-print(*res)
-#word=["apple","banana","cherry"]
-#print(word.upper()
-print("polojitelnoe" if int(input())>0 else "otrisatelnoe")
-print("jup" if int(input())%2==0 else "tak")
-a=[4.-1,7,-3,0,9]
-b=[i if i>=0 else 0 for i in a]
-print(*b)
 
 
