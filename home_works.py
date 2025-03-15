d2bdbb20561bdf53b03a02463363936cc4e03090
Begimaijusupjan <begimaijusupjan@gmail.com>
2025-03-15 07:45:17 +0600

    home work

--- home_works.py
+++ home_works.py
@@ -1,2 +1,32 @@
+def func():
+    print("Hello user")
+f=func()
+f=func()
+f=func()
+def weather():
+    print("Today 15 sunny")
+w=weather()
+def favorate_food():
+    print("My favorate food is soup")
+f=favorate_food
+f()
+def sum_numbers():
+    a=7
+    b=4
+    sum=a+b
+    print(sum)
+s=sum_numbers()
+# def repeat_phrase(text,n=3):
+#     return[text]*3
+#     text="Learning is cool"
+#     print(text)
+# r=repeat_phrase()
+def repeat_phrase():
+    print("Learning is ccol")
+    print("Learning is cool")
+    print('Learning is cool')
+r=repeat_phrase()
+
+
 
 
