a0f4b0cab59c40937de715a7d2868d9d84eb262c
Begimaijusupjan <begimaijusupjan@gmail.com>
2025-03-15 19:38:03 +0600

    it is return

---
+++ return.py
@@ -0,0 +1,12 @@
+# def add(a,b):
+#     return a+b
+#     print(a+b)
+# add(1,2)
+def weather(selsia):
+    if selsia<0:
+        return "it is cool"
+    elif 0<selsia<20:
+        return "warm"
+    else:
+        return "hot"
+#print(weather(-3))
