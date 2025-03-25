68ee11c09ad69408a309c31fd36f63c25315cb59
Begimaijusupjan <begimaijusupjan@gmail.com>
2025-03-25 19:24:02 +0600

    modul

--- home_works.py
+++ home_works.py
@@ -1,3 +1,16 @@
+with open("data_txt","r") as file:
+    lines=file.readlines()
+
+
+with open("filtered_data_txt","w") as filtered_data:
+    for line in lines:
+        if line.strip():
+            filtered_data.write(line)
+
+
+
+
+
 
 
 
