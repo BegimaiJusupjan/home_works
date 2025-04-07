a107980afde9d5e6776424a30387b1d3c5a1a07d
Begimaijusupjan <begimaijusupjan@gmail.com>
2025-04-07 19:19:05 +0600

    ochtu

--- home_works.py
+++ home_works.py
@@ -1,21 +1,6 @@
 
 
 
-import time
-
-
-def give_ticket_number():
-    return str(int(time.time()))
-
-def give_ticket():
-    number_car=int(input("Enter the number of your car: "))
-    if not number_car:
-        print("Number car is not true")
-        return
-    print("number of ticket",number_car,time,time)
-give_ticket_number()
-give_ticket()
-
 
 
 
